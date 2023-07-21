92591cd614e5aefc2288e151810edcf0320bc5b3
Hwan0518 <qip0518@naver.com>
2023-07-21 15:35:26 +0900

    feat: 로그아웃 기능 추가
    AuthController - logOut 기능 추가

--- src/main/java/com/example/cinderella/config/auth/SecurityConfig.java
+++ src/main/java/com/example/cinderella/config/auth/SecurityConfig.java
@@ -11,27 +11,27 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomOAuth2UserService customOAuth2UserService;
-    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+//    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors()
+                .and()
                 .headers().frameOptions().disable()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/login", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/", "/login", "/logout", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/main").hasRole(Role.USER.name())
                 .anyRequest().permitAll()
                 .and()
-                .logout()
-                .logoutSuccessUrl("/login")
-                .and()
+                .formLogin().disable()
                 .oauth2Login()
-                .successHandler(oAuth2SuccessHandler)
-//                .defaultSuccessUrl("/check", true)
+//                .successHandler(oA uth2SuccessHandler)
+                .defaultSuccessUrl("/check", true)
                 .failureUrl("/login")
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService);
